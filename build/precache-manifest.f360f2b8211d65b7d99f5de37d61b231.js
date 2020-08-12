15c6884bc8fac2e027724bca67725faf37c82db5
Patton Parham <patton.parham@verizon.net>
2020-08-12 02:23:43 -0500

    including build to see if this will work with heroku

---
+++ build/precache-manifest.f360f2b8211d65b7d99f5de37d61b231.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa6edf4cfdd2ef05e3dc59d6855b83b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17ca7a77e5db47228d5e",
+    "url": "/static/css/main.f828378a.chunk.css"
+  },
+  {
+    "revision": "35e60debbf24d533daa0",
+    "url": "/static/js/2.ef3ac0ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef3ac0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ca7a77e5db47228d5e",
+    "url": "/static/js/main.10c12524.chunk.js"
+  },
+  {
+    "revision": "8268fde5897383b0a8d3",
+    "url": "/static/js/runtime-main.c53a7c0d.js"
+  }
+]);
