5a4a63b9f785953a79865daa4ec2c231203993a4
Patton Parham <patton.parham@verizon.net>
2020-08-12 11:52:14 -0500

    narrowing down the problem

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../../styles/main.css";
 import "./Admin.css";
-import { storage } from "../../firebase";
+// import { storage } from "../../firebase";
 import {state, useState} from "react";
 
 function Admin(){
