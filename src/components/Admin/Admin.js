6bf88b1815726c822573755a1227a19dc47373a1
Patton Parham <patton.parham@verizon.net>
2020-08-12 09:52:58 -0500

    commented out firebase related stuff to test if it was the deployment issue

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -6,41 +6,41 @@ import {state, useState} from "react";
 
 function Admin(){
 
-    const [image, setImage] = useState(null);
-    const [url, setUrl] = useState("");
-    const [progress, setProgress] = useState(0);
+    // const [image, setImage] = useState(null);
+    // const [url, setUrl] = useState("");
+    // const [progress, setProgress] = useState(0);
 
-    const handleChange = e => {
-        if (e.target.files[0]){
-            setImage(e.target.files[0]);
-        }
-    };
+    // const handleChange = e => {
+    //     if (e.target.files[0]){
+    //         setImage(e.target.files[0]);
+    //     }
+    // };
 
-    const handleUpload = () => {
-        const uploadTask = storage.ref(`images/${image.name}`).put(image);
-        uploadTask.on(
-            "state_changed",
-            snapshot => {
-                const progress = Math.round(
-                    (snapshot.bytestTransferred / snapshot.totalBytes) * 100
-                );
-            },
-            error => {
-                console.log(error);
-            },
-            () => {
-                storage
-                .ref("images")
-                .child(image.name)
-                .getDownloadURL()
-                .then(url => {
-                    setUrl(url);
-                });
-            }
-        )
-    };
+    // const handleUpload = () => {
+    //     const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    //     uploadTask.on(
+    //         "state_changed",
+    //         snapshot => {
+    //             const progress = Math.round(
+    //                 (snapshot.bytestTransferred / snapshot.totalBytes) * 100
+    //             );
+    //         },
+    //         error => {
+    //             console.log(error);
+    //         },
+    //         () => {
+    //             storage
+    //             .ref("images")
+    //             .child(image.name)
+    //             .getDownloadURL()
+    //             .then(url => {
+    //                 setUrl(url);
+    //             });
+    //         }
+    //     )
+    // };
 
-    console.log("image: ", image);
+    // console.log("image: ", image);
 
     return(
 
@@ -57,13 +57,13 @@ function Admin(){
             </div>
             <div className="Messages card w-1/3">
 
-            <button className="uploadbutton" onClick={handleUpload} type="file" >Upload Item</button>
+            {/* <button className="uploadbutton" onClick={handleUpload} type="file" >Upload Item</button>
             <div id="fileinupt" className="">
             <input  type="file"  className="w-full" onChange={handleChange}></input>
             <progress value={progress} max="100"></progress>
             {url}
             <img src={url} alt="firebase-image"></img>
-            </div>
+            </div> */}
 
 
                 
